babd7158e5871a408273ceadb7a5a732a4a34f37
XuTongle <xutongle@msn.com>
2022-03-14 11:46:43 +0800

    Laravel 9

--- src/ObjectStorageServiceProvider.php
+++ src/ObjectStorageServiceProvider.php
@@ -7,7 +7,6 @@
 namespace Larva\Flysystem\Aliyun;
 
 use Illuminate\Filesystem\FilesystemAdapter;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
 use Larva\Flysystem\Oss\OSSAdapter;
@@ -21,11 +20,12 @@ class ObjectStorageServiceProvider extends ServiceProvider
      * Perform post-registration booting of services.
      *
      * @return void
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     public function boot()
     {
-        Storage::extend('oss', function ($app, $config) {
-            $adapter = new OSSAdapter(\config('filesystems.disks.cos'));
+        $this->app->make('filesystem')->extend('oss', function ($app, $config) {
+            $adapter = new OSSAdapter($config);
             return new FilesystemAdapter(new Filesystem($adapter), $adapter);
         });
     }
